a5c00b9898e1f5b4620665fb419c165f528c6c1f
wilson1981 <keeper.chen@gmail.com>
2022-08-25 20:29:42 +0800

    modifi web server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 
 func main() {
-	fmt.Println("hello world!")
+	fmt.Println("web server running...")
+
+	http.HandleFunc("/", helloWorld)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("web server err:", err)
+	}
 }
