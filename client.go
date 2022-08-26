d0abc7ee4f2829647791e1f017ddf90638036702
Carson <jackparadise520a@gmail.com>
2022-08-26 10:24:25 +0800

    自己建立http.NewServeMux()不倚靠http的預設Mux；改用純本機測試

---
+++ client.go
@@ -0,0 +1,27 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://127.0.0.1:8080/hi")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+	fmt.Scanf("")
+}
--- main.go
+++ main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"go-demo1/app"
 	"log"
 	"net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func hi(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi Hello World!")
 }
 
 func main() {
 	fmt.Println("web server running...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // 如果很簡單，你可以把實作直接寫進來，不需要額外再寫個function在外面
+		_, _ = w.Write([]byte("index!"))
+	})
+	mux.HandleFunc("/about/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, app.Version)
+	})
+	mux.HandleFunc("/hi/", hi) // 建議結尾也補上 /
 
-	http.HandleFunc("/", helloWorld)
-
-	err := http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil) // 對外網都有效
+	err := http.ListenAndServe("127.0.0.1:8080", mux) // 純本機，所以防火牆不會彈出來
 	if err != nil {
 		log.Fatal("web server err:", err)
 	}
